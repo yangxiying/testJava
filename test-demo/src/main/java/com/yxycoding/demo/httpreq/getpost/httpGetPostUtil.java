ba9743a469902d673f82cc74e23e108b65fe8ab5
yangxya <yangxiying.2010@foxmail.com>
2020-01-12 17:26:58 +0800

    zipkin学习

--- test-demo/src/main/java/com/yxycoding/demo/httpreq/getpost/httpGetPostUtil.java
+++ test-demo/src/main/java/com/yxycoding/demo/httpreq/getpost/httpGetPostUtil.java
@@ -1,4 +1,19 @@
 package com.yxycoding.demo.httpreq.getpost;
 
+import sun.net.www.http.HttpClient;
+
 public class httpGetPostUtil {
+
+
+    /**
+     * import org.apache.commons.httpclient.HttpClient;
+     * import org.apache.commons.httpclient.HttpStatus;
+     * import org.apache.commons.httpclient.NameValuePair;
+     * import org.apache.commons.httpclient.methods.PostMethod;
+     */
+    public void test(){
+//        HttpClient httpClient = new HttpClient();
+//        PostMethod method = new PostMethod(url);
+    }
+
 }
--- test-demo/src/main/java/com/yxycoding/demo/httpreq/getpost/httptest.java
+++ test-demo/src/main/java/com/yxycoding/demo/httpreq/getpost/httptest.java
@@ -1,4 +1,28 @@
 package com.yxycoding.demo.httpreq.getpost;
 
+import cn.hutool.http.HttpRequest;
+import cn.hutool.http.HttpResponse;
+import cn.hutool.http.HttpUtil;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.HashMap;
+
+@Slf4j
 public class httptest {
+
+    public static void main(String[] args) {
+        HashMap<String, Object> paramMap = new HashMap<>();
+        paramMap.put("city", "北京");
+
+        String url ="http://10.34.1.199/Home/Login?userName=HZMDZD&userPass=123456";
+        String result= HttpUtil.post(url, paramMap);
+        log.info("==");
+
+
+        HttpResponse res = HttpRequest.post(url).execute();
+        res.header("Set-Cookie");
+        res.body();
+        log.info("ssss");
+
+    }
 }
